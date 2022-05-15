16415efbe43b197e24c4b617e7735a588eac4459
Aabid Roshan <aabidr72@umd.edu>
2022-05-15 16:30:32 -0400

    Project functionality complete.

--- apiModule.js
+++ apiModule.js
@@ -3,10 +3,9 @@ const axios = require('axios');
 async function getActivityName() {
     try {
         const res = await axios.get('http://www.boredapi.com/api/activity')
-        console.log(res.data.activity)
         return res.data.activity
     } catch(e) {
-        console.log("Error occurred while using APIs.")
+        console.log("Error occurred while using Bored API.")
         console.error(e)
     }
 }
--- mongoModule.js
+++ mongoModule.js
@@ -7,6 +7,7 @@ const dbName = process.env.MONGO_DB_NAME;
 const collectionName = process.env.MONGO_COLLECTION;
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { name } = require("ejs");
 
 const databaseAndCollection = {db: dbName, collection: collectionName};
 const uri = `mongodb+srv://${userName}:${password}@cluster0.7hqzy.mongodb.net/${dbName}?retryWrites=true&w=majority`;
@@ -41,4 +42,30 @@ async function getAll() {
     }
 }
 
-module.exports = {insertApplication, getAll}
+async function deleteActivities(activities, deleteAllActivities) {
+    let count = 0
+    try {
+        await client.connect();
+        let result = ""
+        if (deleteAllActivities == "on") {
+            result = await client.db(databaseAndCollection.db)
+                    .collection(databaseAndCollection.collection)
+                    .deleteMany({})
+            count = result.deletedCount;
+        } else if (activities != null) {
+            for (const act of activities) {
+                await client.db(databaseAndCollection.db)
+                    .collection(databaseAndCollection.collection)
+                    .deleteOne({name: act})
+            }
+            count = activities.length;
+        }
+        return count;
+    } catch (e) {
+        console.log(e);
+    } finally {
+        await client.close();
+    }
+}
+
+module.exports = {insertApplication, getAll, deleteActivities}
